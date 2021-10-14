350157ddc93af2553a2316c92ce8aa4743b4d543
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-10-14 14:45:52 -0400

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -25,6 +25,7 @@ cp /root/sources.list.bak /etc/apt/sources.list -r;
 else yum groupinstall xfce -y && yum install tigervnc-server expect novnc -y;
 fi;
 mkdir -p ~/.vnc ;
+if [ ! -f ~/.vnc/passwd ]; then
 /usr/bin/expect <<EOF
 spawn /usr/bin/vncserver :55 -localhost
 expect "Password:"
@@ -35,7 +36,9 @@ expect "Would you like to enter a view-only password (y/n)?"
 send "n\r"
 expect eof
 exit
-EOF
+EOF;
+else /usr/bin/vncserver :55 -localhost ;
+fi;
 DISPLAY=:55 xfce4-session &
 if [ -f /usr/bin/apt ]; then /usr/share/novnc/utils/launch.sh --listen $randport --vnc localhost:5955 & fi;
 if [ -f /usr/bin/yum ]; then novnc_server --listen $randport --vnc localhost:5955 --web /usr/share/novnc & fi;
